c2b35ca44d5dc3968aa02978d400520e38fdc318
yellowunicorn <yellowunicorn001@gmail.com>
2017-05-11 21:13:49 +0200

    1rst version

---
+++ TalkToMe/TalkToMe/TallGuy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalkToMe
+{
+    class TallGuy
+    {
+        public string Name;
+        public int Height;
+
+        public void TalkAboutYourself()
+        {
+            Console.WriteLine("Nazywam się " + Name + " i mam " + Height + " wzrostu");
+        }
+    }
+}
